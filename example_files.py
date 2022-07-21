973828d0a8e773f5caf4936a58a357aafd3101c0
Vit Ruzicka <previtus@gmail.com>
2022-07-21 22:51:10 +0000

    initial files

---
+++ example_files.py
@@ -0,0 +1,8 @@
+# Sentinel-2 files:
+
+# !gdown 1UCNnxaL9pQSkkZQx0aDEWQL0UBXPXkv0 -O fires.zip
+example_FDLravaen_S2_L1C_file = "/home/previtus/data/fires/fire_riveaux/S2/2019-02-24.tif"
+example_FDLravaen_S2_L1C_folder = "/home/previtus/data/fires/fire_riveaux/S2/"
+
+example_FDLWildfires_S2_Lidk_file = "/home/previtus/data/mesa_two_files/S2B_20180815.tif"
+example_FDLWildfires_S2_Lidk_folder = "/home/previtus/data/mesa_two_files/"
